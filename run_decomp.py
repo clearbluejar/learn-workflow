543082200152eb072ef7d53b483c4565a1b3a3a0
clearbluejar <3752074+clearbluejar@users.noreply.github.com>
2023-03-31 16:47:29 -0400

    another3

---
+++ run_decomp.py
@@ -0,0 +1,64 @@
+from pathlib import Path
+import argparse
+import json
+from concurrent.futures import ThreadPoolExecutor  # pip install futures
+from concurrent import futures
+from subprocess import STDOUT, call
+import multiprocessing
+
+
+def rm_tree(pth):
+    pth = Path(pth)
+    for child in pth.glob('*'):
+        if child.is_file():
+            child.unlink()
+        else:
+            rm_tree(child)
+    pth.rmdir()
+
+
+# LIMIT = multiprocessing.cpu_count()
+LIMIT = 2
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--id", type=int, help="fileid to download from", required=True)
+parser.add_argument("--fl_path", help="File List Path", default='bins/meta')
+
+
+args = parser.parse_args()
+
+file_list_path = Path(args.fl_path) / f'dl_files{args.id}.json'
+
+
+files_info_list = json.loads(file_list_path.read_bytes())
+
+file_paths = []
+for status, file, pe_ver, file_path in files_info_list:
+
+    print(f"path... {file_path}")
+
+    file_paths.append(Path(file_path))
+
+log_path = file_list_path.parent.parent / 'decomp_logs'
+# fresh logs
+if log_path.exists():
+    rm_tree(log_path)
+log_path.mkdir(exist_ok=True)
+
+
+with ThreadPoolExecutor(max_workers=LIMIT) as executor:
+
+    log_paths = []
+    for file_path in file_paths:
+        file_log_path = log_path / f'{file_path.name}.log'
+        cmd = f'ghidrecomp {file_path}'
+        # cmd = f'pwd'
+
+        log_paths.append([file_log_path, cmd])
+
+    future_sub = (executor.submit(call, cmd, stdout=log.open('w'), stderr=STDOUT, shell=True)
+                  for log, cmd in log_paths)
+
+    for future in futures.as_completed(future_sub):
+        print(future.result())
