855c31e06a24e995ccffbb62cb0a8bbe2af42b72
clearbluejar <3752074+clearbluejar@users.noreply.github.com>
2023-05-16 09:51:25 -0400

    another9

--- gen_files.py
+++ gen_files.py
@@ -13,11 +13,17 @@ sources = merged_df['source'].explode().unique()
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--include", nargs='+', action='extend', help="filter key=value")
-parser.add_argument("--exclude",  nargs='+', action='extend', help="filter key=value")
-parser.add_argument("--count", type=int, help="number of files to generate", required=True)
+parser.add_argument("--include", nargs='+',
+                    action='extend', help="filter key=value")
+parser.add_argument("--exclude",  nargs='+',
+                    action='extend', help="filter key=value")
+parser.add_argument("--count", type=int,
+                    help="number of files to generate", required=True)
+parser.add_argument("--require-proc-info",
+                    help="only use files with extra info", action='store_true')
 parser.add_argument("--source", choices=sources, default=sources[0])
-parser.add_argument("--limit-list", help="limit length of file lists", type=int)
+parser.add_argument(
+    "--limit-list", help="limit length of file lists", type=int)
 
 
 args = parser.parse_args()
@@ -50,19 +56,30 @@ for key, val in includes:
     assert key in df.columns, f'{key}: not found in {df.columns} check - -includes'
     val_type = type(df[key].iloc[0])
     if val_type == list:
-        df = df[df[key].apply(lambda x: any([val.lower() in item.lower() for item in x]))]
+        df = df[df[key].apply(lambda x: any(
+            [val.lower() in item.lower() for item in x]))]
     else:
         df = df[df[key] == val]
     print(df.shape[0])
 
 
 df = df.sort_values(by='size', ascending=True)
+df.reset_index(names='sha256', inplace=True)
+
+proc_df = pd.read_json(PROCMON_MODLOAD_JSON_PATH)
+proc_df = proc_df.reset_index(names=['Path'])
+proc_df['is_priv'] = proc_df['User'].astype(
+    str).str.contains('SYSTEM|SERVICE|LOCAL')
+
+df = df.rename(columns={'VersionInfo.FileName': 'Path'})
+merged_df = proc_df.merge(df, on='Path', how='inner')
 
 print(df.head())
 
-all_files_list = df.to_dict(orient='records')
-
-print(len(all_files_list))
+if args.require_proc_info:
+    all_files_list = merged_df.to_dict(orient='records')
+else:
+    all_files_list = df.to_dict(orient='records')
 
 bucket = {}
 # items sorted by size.. file the buckets evenly
@@ -90,12 +107,12 @@ for i, files in bucket.items():
     print(f'Writing file: {path} with count: {len(bucket[i])}')
     path.write_text(json.dumps(bucket[i]))
 
-print(f'Final file count: {df.shape[0]}')
-
 sources_values = df['source'].value_counts()
-paths_values = df['VersionInfo.FileName'].apply(lambda x: PureWindowsPath(x).parent).value_counts()
+paths_values = df['Path'].apply(
+    lambda x: PureWindowsPath(x).parent).value_counts()
 
-with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+# more options can be specified also
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):
 
     print(sources_values)
     print(paths_values)
@@ -110,27 +127,21 @@ paths_values_path = meta_path / 'paths_values.md'
 paths_values.to_markdown(paths_values_path)
 
 df_path = meta_path / 'pandas_query_df.json'
-df.reset_index(names='sha256', inplace=True)
 df.to_json(df_path)
 
 df_proc_md_path = meta_path / 'proc_pandas_query.md'
 df_proc_path = meta_path / 'proc_pandas_query.json'
 
-
-proc_df = pd.read_json(PROCMON_MODLOAD_JSON_PATH)
-proc_df = proc_df.reset_index(names=['Path'])
-proc_df['is_priv'] = proc_df['User'].astype(str).str.contains('SYSTEM|SERVICE|LOCAL')
-
-df = df.rename(columns={'VersionInfo.FileName': 'Path'})
-merged_df = proc_df.merge(df, on='Path', how='inner')
 merged_df.to_markdown(df_proc_md_path, tablefmt="github")
 merged_df.to_json(df_proc_path)
 
 x_sources_values = merged_df['source_x'].value_counts()
 y_sources_values = merged_df['source_y'].value_counts()
-paths_values = merged_df['Path'].apply(lambda x: PureWindowsPath(x).parent).value_counts()
+paths_values = merged_df['Path'].apply(
+    lambda x: PureWindowsPath(x).parent).value_counts()
 
-with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+# more options can be specified also
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):
 
     print(x_sources_values)
     print(y_sources_values)
@@ -143,3 +154,9 @@ pd.read_json(df_path)
 
 args_path = meta_path / 'args.json'
 args_path.write_text(json.dumps(vars(args)))
+
+
+print(f'Final query file count: {df.shape[0]}')
+print(f'Final query with proc_info file count: {merged_df.shape[0]}')
+print(f'Generated files: {len(all_files_list)}')
+print(f'Files per worker : {int(len(all_files_list)/args.count)}')
