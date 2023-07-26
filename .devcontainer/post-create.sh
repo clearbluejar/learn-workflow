afa6cc92015856d8d6c6ef27d8a307d1d8eacdad
clearbluejar <3752074+clearbluejar@users.noreply.github.com>
2023-07-26 21:00:12 +0000

    update image

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,8 +4,3 @@ source .env/bin/activate
 
 # Install pyhdira
 pip install ghidrecomp
-
-# If arm64 os, need to build native binaries for Ghidra
-if uname -a | grep -q 'aarch64'; then
-    $GHIDRA_INSTALL_DIR/support/buildNatives
-fi
