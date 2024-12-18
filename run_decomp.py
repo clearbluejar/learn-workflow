4d49ae04ede5c9954c4945bbffd2a3c9a802ee68
clearbluejar <3752074+clearbluejar@users.noreply.github.com>
2024-12-18 15:27:37 +0000

    update

--- run_decomp.py
+++ run_decomp.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 import argparse
 import json
-from concurrent.futures import ThreadPoolExecutor  # pip install futures
+from concurrent.futures import ThreadPoolExecutor
 from concurrent import futures
-from subprocess import STDOUT, call
+from subprocess import STDOUT, call, TimeoutExpired
 
 
 def rm_tree(pth):
@@ -21,6 +21,7 @@ parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFo
 parser.add_argument("--id", type=int, help="fileid to download from", required=True)
 parser.add_argument("--fl_path", help="File List Path", default='bins/meta')
 parser.add_argument("--proc-limit", help="concurrent process limit", type=int, default=2)
+parser.add_argument("--timeout", help="Timeout for subprocess calls in seconds", type=int, default=1200)
 
 
 args = parser.parse_args()
@@ -44,17 +45,31 @@ if log_path.exists():
 log_path.mkdir(exist_ok=True)
 
 
+def run_command(cmd, log, timeout):
+    try:
+        print(f"Running cmd: {cmd} with timeout:{timeout} log: {log}")
+        result = call(cmd, stdout=log.open('w'), stderr=STDOUT, shell=True, timeout=timeout)
+        return f"Command '{cmd}' success with result: {result}"
+    except TimeoutExpired:
+        return f"{"*"*30}\nCommand '{cmd}' timed out. Log details:{get_last_x_lines(log,20)}\n{"*"*30}"
+
+def get_last_x_lines(file_path,x):
+    msg = f"Last {x} lines of {file_path}:\n"
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        last_lines = lines[-x:]
+        msg += "\n".join(last_lines)
+    return msg
+
 with ThreadPoolExecutor(max_workers=args.proc_limit) as executor:
 
     log_paths = []
     for file_path in file_paths:
         file_log_path = log_path / f'{file_path.name}.log'
-        cmd = f'ghidrecomp --va {file_path} --bsim'
-        # cmd = f'pwd'
-
+        cmd = f'ghidrecomp --va {file_path} --bsim --gzf'
         log_paths.append([file_log_path, cmd])
 
-    future_sub = (executor.submit(call, cmd, stdout=log.open('w'), stderr=STDOUT, shell=True)
+    future_sub = (executor.submit(run_command, cmd, log, args.timeout)
                   for log, cmd in log_paths)
 
     for future in futures.as_completed(future_sub):
